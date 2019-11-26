c3510119fbe2bd780070f8149428553d4a1cf620
Vadim Falin <VFalin@domain.corp>
2019-11-26 16:43:45 +0300

    build

---
+++ build/precache-manifest.a938fd72226ece6d830064cbc001aa29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ab3d33aef1104603dc497641f75b57",
+    "url": "https://major666rus.github.io/react-football/build/index.html"
+  },
+  {
+    "revision": "f05b877bc31465de746e",
+    "url": "https://major666rus.github.io/react-football/build/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "38ad46c3c7d944ae8ca0",
+    "url": "https://major666rus.github.io/react-football/build/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f05b877bc31465de746e",
+    "url": "https://major666rus.github.io/react-football/build/static/js/2.98eef703.chunk.js"
+  },
+  {
+    "revision": "38ad46c3c7d944ae8ca0",
+    "url": "https://major666rus.github.io/react-football/build/static/js/main.8a58f71b.chunk.js"
+  },
+  {
+    "revision": "855912b095e8f0e2412b",
+    "url": "https://major666rus.github.io/react-football/build/static/js/runtime-main.c78cd9ca.js"
+  }
+]);
