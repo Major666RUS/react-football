682c1c5daf1ecf946cf0ac6f599827ae5e3fa557
Vadim Falin <VFalin@domain.corp>
2019-12-04 10:56:54 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react'
 
 function App() {
@@ -12,27 +13,19 @@ export default App
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+=======
+import React from 'react'
+>>>>>>> first
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>Home</h1>
+  )
 }
 
+<<<<<<< HEAD
 export default App;
 >>>>>>> Initial commit from Create React App
+=======
+export default App
+>>>>>>> first
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> first
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Route, Link, Switch, HashRouter as Router } from 'react-router-dom'
@@ -7,6 +10,7 @@ import './index.css'
 import App from './App'
 import List from './List'
 import Team from './Team'
+<<<<<<< HEAD
 import NotFound from './NotFound'
 import * as serviceWorker from './serviceWorker'
 
@@ -31,12 +35,33 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 >>>>>>> Initial commit from Create React App
+=======
+// import NotFound from './NotFound'
+import * as serviceWorker from './serviceWorker'
+
+const routing = (
+    <Router>
+      <Link to="/teams">TEAMS</Link>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/teams" component={List} />
+        <Route path="/teams/:id" component={Team} />
+        {/* <Route component={NotFound} /> */}
+      </Switch>
+    </Router>
+  )
+  ReactDOM.render(routing, document.getElementById('root'))
+>>>>>>> first
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 <<<<<<< HEAD
+<<<<<<< HEAD
 serviceWorker.unregister()
 =======
 serviceWorker.unregister();
 >>>>>>> Initial commit from Create React App
+=======
+serviceWorker.unregister()
+>>>>>>> first
